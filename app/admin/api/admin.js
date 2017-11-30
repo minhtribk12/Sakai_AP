e703131869459f783cd24cdb546d24fb9a243e4b
DuyThai <duythai1912@gmail.com>
2017-11-30 22:10:42 +0700

    Minimize source: add dao

--- app/admin/api/admin.js
+++ app/admin/api/admin.js
@@ -1,26 +1,12 @@
-var mysql = require('mysql');
-var fs = require('fs');
-var db = require(__base + 'config/database');
-var config = require(__base + 'config/local_config');
+var dao = require(__base + 'app/dao');
 
 module.exports = function (app) {
-     app.post('/api/admin/login', function (req, res) {
-        var pool = mysql.createPool(db);
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                pool.end();
-                res.json({});
-                console.log({ "code" : err.code, "status" : err.message });
-                return;
-            } else {
-
-                connection.query('SELECT users_id, name, email FROM users WHERE users_id = ? AND password = ?', [req.body.username, req.body.passwd], function (err, rows) {
-                    connection.destroy();
-                    pool.end();
-                    res.json(rows);
-                });
-            }
-        });
+    app.post('/api/admin/login', function (req, res) {
+        var sql = 'SELECT users_id, name, email FROM users WHERE users_id = ? AND password = ?';
+        var params = [req.body.username, req.body.passwd];
+        dao.query(sql, params, function(data){
+            res.json(data);
+        })
     });
 
     
---
+++ app/dao.js
@@ -0,0 +1,22 @@
+﻿var mysql = require('mysql');
+var db = require(__base + 'config/database');
+var config = require(__base + 'config/local_config');
+
+exports.query = function (queryString, params, callback) {
+    var pool = mysql.createPool(db);
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                pool.end();
+                callback({});
+                console.log({ "code" : err.code, "status" : err.message });
+                return;
+            } else {
+
+                connection.query(queryString, params, function (err, rows) {
+                    connection.destroy();
+                    pool.end();
+                    callback(rows);
+                });
+            }
+        });
+};
